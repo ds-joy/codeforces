b089b5e144894c4919acd272b0607358fe47beb9
Your Name <you@example.com>
2020-03-16 12:36:12 +0600

    problem a accepted

---
+++ div3_555/a_Reachable_numbers.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    long long n, count;
+
+    cin >> n;
+
+    long long arr[1000];
+    count = 1;
+    arr[count-1] = n;
+
+    while(1) {
+        count++;
+        n=n+1;
+
+        while(n%10 == 0){
+            n = n/10;
+        }
+
+        for(int i = 0; i<count-1; i++) {
+            if(arr[i] == n) {
+                cout << count-1 << endl;
+                return 0;
+            }
+            else {
+                arr[count-1] = n;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
+
