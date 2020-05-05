3448da37f975f98309cc071835392c8352b6a391
Debasish Sarker <debasishsarker36@gmail.com>
2020-05-05 22:48:26 +0600

    c accepted

--- div2_638/c.cpp
+++ div2_638/c.cpp
@@ -12,33 +12,31 @@ int main()
     {
         cin >> n >> k;
         cin >> str;
-        string arr[k]={};
+        sort(str.begin(), str.end());
 
-        sort(str.begin(),str.end());
-
-        if(k==1)
+        if(str[0] != str[k-1])
         {
-            cout << str << endl;
+            cout << str[k-1] << endl;
+            continue;
+        }
+
+        cout << str[0];
+
+        if(str[k] != str[n-1])
+        {
+            for(int i=k; i<n; i++)
+                cout << str[i];
+            cout << endl;
+
         }
         else
         {
-            for (ll i = 0; i < n; i++)
-            {
-                if( n>3 && i == (n-2) && (arr[(i-1)%k] == (arr[i%k]+str[i])))
-                {
-                    arr[(i-1)%k] += str[i];
-                    arr[(i-1)%k] += str[i+1];
-                    break;
-                }
-                else
-                {
-                    arr[i%k] += str[i];
-                }
-            }
+            for(int i=0; i<(n-1)/k; i++)
+                cout << str[n-1];
+            cout << endl;
 
-            sort(arr,arr+k);
-            cout << arr[k-1] << endl;
         }
+
     }
 
     return 0;
---
+++ randomProblems/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+
+    // Declare second integer, double, and String variables.
+    int secondInt;
+    double secondDouble;
+    string secondString;
+
+    // Read and save an integer, double, and String to your variables.
+    cin >> secondInt >> secondDouble;
+    cin.ignore();
+    getline(cin, secondString);
+    cout << secondString << endl;
+
+    // Print the sum of both integer variables on a new line.
+   cout << i+secondInt << endl;
+
+    // Print the sum of the double variables on a new line.
+   cout <<fixed <<(d+secondDouble) << endl;
+
+    // Concatenate and print the String variables on a new line
+    cout << s+secondString << endl;
+
+    // The 's' variable above should be printed first.
+    return 0;
+}
