64df5e77c09e88fd5b55bdcc4a41ee18cc66d23f
Debasish Sarker <debasishsarker36@gmail.com>
2020-08-05 23:25:53 +0600

    520b wrong ans on test 3

---
+++ randomProblems/520b.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ll given, desired;
+    cin >> given >> desired;
+
+    if(desired <= given) {
+        cout << (given - desired) << endl;
+        return 0;
+    }
+
+    if(desired >= 2*given)
+    {
+        ll a = desired / given;
+        ll b = desired % given;
+
+        cout << a+b << endl;
+        return 0;
+    }
+
+    ll a = desired/2;
+    ll ans = (given-a) + (desired - 2*a) + 1;
+    cout << ans << endl;
+
+
+}
